a4f73a0726aaf2a437b6ee7df304ca8f9a8a159e
Jean C. Contreras <jean.cntrs@gmail.com>
2020-10-02 11:16:05 -0300

    A unit test was written to the form component

---
+++ src/components/Form.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Form from './Form';
+
+configure({ adapter: new Adapter() });
+
+describe('Form', () => {
+    test('Add finance', () => {
+        const agregarFinanza = jest.fn();
+        const prevent = jest.fn();
+        const wrapper = shallow(<Form agregarFinanza={agregarFinanza} />);
+
+        wrapper.find('input').at(0).simulate('change', { target: { value: 'description' } });
+        wrapper.find('input').at(1).simulate('change', { target: { value: '150' } });
+        wrapper.find('form').simulate('submit', { preventDefault: prevent });
+
+        expect(agregarFinanza.mock.calls).toEqual([
+            [{ desc: 'description', cant: 150 }]
+        ]);
+        expect(prevent.mock.calls).toEqual([[]]);
+    });
+});
